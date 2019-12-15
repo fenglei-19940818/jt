3e96d591e8a7fccc9a6b3b96057e0d56b58025c0
fenglei <767596190@qq.com>
2019-12-15 18:53:36 +0800

    修改配置文件

--- jt-manage/src/main/java/com/jt/SpringBootRun.java
+++ jt-manage/src/main/java/com/jt/SpringBootRun.java
@@ -10,7 +10,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class SpringBootRun {
 	
 	public static void main(String[] args) {
-		
+		System.out.println();
 		SpringApplication.run(SpringBootRun.class, args);
 	}
 }
