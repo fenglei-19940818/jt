2c44ef215fbd9baca5671d34411ff9bccfaacdb5
fenglei <767596190@qq.com>
2019-12-23 19:56:26 +0800

    完成订单项目代码

--- jt-order/src/main/java/com/jt/service/DubboOrderServiceImpl.java
+++ jt-order/src/main/java/com/jt/service/DubboOrderServiceImpl.java
@@ -1,13 +1,21 @@
 package com.jt.service;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.jt.mapper.OrderItemMapper;
 import com.jt.mapper.OrderMapper;
 import com.jt.mapper.OrderShippingMapper;
+import com.jt.pojo.Order;
+import com.jt.pojo.OrderItem;
+import com.jt.pojo.OrderShipping;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class DubboOrderServiceImpl implements DubboOrderService {
+
     @Autowired
     private OrderMapper orderMapper;
     @Autowired
@@ -15,5 +23,45 @@ public class DubboOrderServiceImpl implements DubboOrderService {
     @Autowired
     private OrderItemMapper orderItemMapper;
 
-    
+    @Override
+    @Transactional
+    public String saveOrder(Order order) {
+        //生成订单id
+        String orderId = "" + order.getUserId() + System.currentTimeMillis();
+        order.setOrderId(orderId).setStatus(1);
+        //订单入库
+        orderMapper.insert(order);
+        System.out.println("订单录入成功");
+        OrderShipping shipping = order.getOrderShipping();
+        shipping.setOrderId(orderId);
+        //订单物流入库成功
+        orderShippingMapper.insert(shipping);
+        System.out.println("订单物流入库成功");
+        List<OrderItem> orderItems = order.getOrderItems();
+        //订单商品入库成功
+        for (OrderItem orderItem : orderItems) {
+            orderItem.setOrderId(orderId);
+            orderItemMapper.insert(orderItem);
+        }
+        System.out.println("订单商品入库成功");
+        return orderId;
+    }
+
+    @Override
+    public Order findOrderByID(String orderId) {
+        //查询order
+        Order order = orderMapper.selectById(orderId);
+        //查询orderShipping
+        OrderShipping shipping = orderShippingMapper.selectById(orderId);
+        //查询orderItem
+        QueryWrapper queryWrapper = new QueryWrapper();
+        queryWrapper.eq("order_id", orderId);
+        List<OrderItem> orderItems = orderItemMapper.selectList(queryWrapper);
+        if (order != null && shipping != null && orderItems != null) {
+            order.setOrderShipping(shipping);
+            order.setOrderItems(orderItems);
+            return order;
+        }
+        return null;
+    }
 }
--- jt-web/src/main/java/com/jt/controller/OrderController.java
+++ jt-web/src/main/java/com/jt/controller/OrderController.java
@@ -2,11 +2,15 @@ package com.jt.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.jt.pojo.Cart;
+import com.jt.pojo.Order;
 import com.jt.service.DubboCartService;
+import com.jt.service.DubboOrderService;
 import com.jt.util.UserThreadLocal;
+import com.jt.vo.SysResult;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -17,12 +21,40 @@ public class OrderController {
     @Reference
     private DubboCartService dubboCartService;
 
+    @Reference
+    private DubboOrderService dubboOrderService;
+
     @RequestMapping(value = "/create")
     public String create(Model model) {
         Long userId = UserThreadLocal.get().getId();
         List<Cart> cartList = dubboCartService.findCartListByUserId(userId);
         model.addAttribute("carts", cartList);
         return "order-cart";
+    }
+
+    /**
+     * 订单提交
+     *
+     * @param order
+     * @return
+     */
+    @RequestMapping(value = "/submit")
+    @ResponseBody
+    public SysResult saveOrder(Order order) {
+        SysResult sysResult = new SysResult();
+        //获取用户Id
+        Long userId = UserThreadLocal.get().getId();
+        order.setUserId(userId);
+        String orderId = dubboOrderService.saveOrder(order);
+        //删除购物车的内容
+        return sysResult.setData(orderId);
+    }
+
+    @RequestMapping(value = "/success")
+    public String findOrderByID(String id, Model model) {
+        Order order = dubboOrderService.findOrderByID(id);
+        model.addAttribute("order", order);
+        return "success";
     }
 
 }
